22e931535e0de4f73303533c001c400ed59d47d0
Victor Basso <vitobasso@gmail.com>
2019-08-27 19:58:07 +0100

    unit test

--- test/scraper/main_test.clj
+++ test/scraper/main_test.clj
@@ -3,5 +3,9 @@
             [scraper.main :refer :all]))
 
 (deftest test-parse-selector-word
-  (testing "Single tag"
-    (is (= :div (parse-selector-word "div")))))
+  (testing "single tag"
+    (is (= :div (parse-selector-word "div"))))
+  (testing "tag with nth-child"
+    (let [[tag fun] (parse-selector-word "div:nth-child(1)")]
+      (is (= :div tag) (function? fun))))
+  )
