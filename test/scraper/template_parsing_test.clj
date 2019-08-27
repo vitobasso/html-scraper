8cd2af015b7199d744cc5a7a224fe7987690c856
Victor Basso <vitobasso@gmail.com>
2019-08-27 20:46:37 +0100

    simplified template. added a test.

--- test/scraper/template_parsing_test.clj
+++ test/scraper/template_parsing_test.clj
@@ -3,8 +3,10 @@
             [scraper.template-parsing :refer :all]))
 
 (deftest test-parse-selector-word
-  (testing "single tag"
+  (testing "simple tag"
     (is (= :div (parse-selector-word "div"))))
+  (testing "tag with class"
+    (is (= :div.a-class (parse-selector-word "div.a-class"))))
   (testing "tag with nth-child"
     (let [[tag fun] (parse-selector-word "div:nth-child(1)")]
       (is (= :div tag) (function? fun))))
