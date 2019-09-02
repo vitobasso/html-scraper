37e4859e3603cd339451f361d77f0c571546f329
Victor Basso <vitobasso@gmail.com>
2019-09-02 22:54:29 +0100

    deps.edn

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject papershop-html-scraper "0.1.0"
-  :description "Papershop HTML Scraper"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.forward/yaml "1.0.9"]
-                 [clj-http "3.10.0"]
-                 [enlive "1.1.6"]]
-  )
