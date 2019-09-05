ff4887b46c33ec5855e99a1e507e3eb25dbc1664
Victor Basso <vitobasso@gmail.com>
2019-09-05 11:26:16 +0100

    hickory bug fix: class separated by linefeed

--- src/scraper/main.clj
+++ src/scraper/main.clj
@@ -11,7 +11,7 @@
       (println attribute))))
 
 (def config
-  (scraping/parse-config (yaml/from-file "templates/amazon-co-uk.yml")))
+  (scraping/parse-config (yaml/from-file "templates/booking.yml")))
 
 (defn page [search-term page-number]
   (print-items (scraping/scrape-list search-term page-number config)))
@@ -20,13 +20,11 @@
   (print-items (scraping/scrape-detail item config)))
 
 ; uncomment to try manual scraping from repl
-(def list-config (:list-page config))
-(def url-template (:url list-config))
-(def url (scraping/build-search-url url-template "cardboard boxes" 1))
-(def html (-> url client/get :body h/parse h/as-hickory))
-(def items (s/select (:item-selector list-config) html))
-(def item (first items))
-(print-items (scraping/scrape-item item list-config))
+;(def list-config (:list-page config))
+;(def url-template (:url list-config))
+;(def url (scraping/build-search-url url-template "london" 1))
+;(def html (-> url client/get :body h/parse h/as-hickory))
+;(def items (s/select (:item-selector list-config) html))
+;(def item (first items))
+;(print-items (scraping/scrape-item item list-config))
 ;(print-items (scraping/scrape-items html config))
-
-;(-> (s/select (s/child (s/class "blabla")) site-htree) first :content first string/trim)
