5de8f2637cc72cb5145ec81f28e2d159a0c81e3d
Victor Basso <victorbasso@Victors-MacBook-Pro.local>
2024-03-28 16:48:48 -0300

    update deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [hickory "0.7.1"]
                  [com.github.vitobasso/hickory-css-selector "custom-for-papershop-SNAPSHOT"]
-                 [io.forward/yaml "1.0.9"]
+                 [io.forward/yaml "1.0.11"]
                  [clj-http "3.10.0"]]
   :repositories {"jitpack" {:url "https://jitpack.io"}}
   :test-selectors {:default (complement :remote)
